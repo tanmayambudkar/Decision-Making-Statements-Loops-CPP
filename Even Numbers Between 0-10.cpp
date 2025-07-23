1145aaffdff47f86a01853831589505f2e03c02b
Tanmay Ambudkar <tanmayambudkar@gmail.com>
2025-07-23 09:19:36 +0530

    Add files via upload

---
+++ Even Numbers Between 0-10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i;
+    int n=10;
+    cout<<"The Even Numbers Between 0-10 are";
+    for(i=0;i<=n;i=i+2){
+        cout<<"\n"<<i;
+    }
+    return 0;
+}

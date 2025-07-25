462d9858d691c169885624c081820add854f2e00
Tanmay Ambudkar <tanmayambudkar@gmail.com>
2025-07-25 15:33:37 +0530

    Add files via upload

---
+++ Mirror Inverted Star Pattern.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j,k,n=5;
+    for(i = 1; i <= n; i++) {
+        for(k=1;k<i;k++)
+        {
+            cout<<"  ";
+        }
+        for(j=i; j<=n ; j++) {
+            cout << " *" ; 
+        }
+        
+        
+        cout << endl ;
+    }
+}

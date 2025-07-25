3644d32495c705c174e7247a2878872d01a1783b
Tanmay Ambudkar <tanmayambudkar@gmail.com>
2025-07-25 11:40:29 +0530

    Add files via upload

---
+++ Floyd's Series.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i,j,n=5,z=1;
+    for(i=1;i<n;i++){
+        for(j=1;j<i+1;j++){
+            cout<<" "<<z;
+            z++;
+        }
+        cout<<"\n";
+    }
+}

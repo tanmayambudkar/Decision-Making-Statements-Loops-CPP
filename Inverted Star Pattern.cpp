231d8dcf16395bcadc9f7e0fa174a6f90867b715
Tanmay Ambudkar <tanmayambudkar@gmail.com>
2025-07-25 11:40:55 +0530

    Add files via upload

---
+++ Inverted Star Pattern.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i,j,n=5;
+    for(i=5;i<=n;i--){
+        for(j=1;j<=i;j++){
+            cout<<" *";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
